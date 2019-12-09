2039f6df0893f3998089c94374b91c0ef5776601
Anthony Mai <anthony.mai530@gmail.com>
2019-12-09 02:19:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,32 +1,7 @@
-Skip to content
-Search or jump to…
 
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@antimonysayz 
-Learn Git and GitHub without any code!
-Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
-
-
-1
-010alexisadorn/Flatiron-OO-Ruby
- Code Issues 0 Pull requests 0 Actions Projects 0 Wiki Security Insights
-Flatiron-OO-Ruby/oo-cash-register-v-000/lib/cash_register.rb / 
-@alexisadorn alexisadorn oo-cash-register-v-000
-1ebac13 on May 17, 2017
-39 lines (34 sloc)  748 Bytes
-  
-Code navigation is available!
-Navigate your code with ease. Click on function and method calls to jump to their definitions or references in the same repository. Learn more
-
-You're using code navigation to jump to definitions or references.
-Learn more or give us feedback
-require 'pry'
 class CashRegister
-  attr_accessor :total, :discount, :price, :items
+
+  attr_accessor :total, :discount, :last_transaction_amount, :items
 
   def initialize(discount = 0)
     @total = 0
@@ -34,44 +9,36 @@ class CashRegister
     @items = []
   end
 
-  def add_item(item, price, quantity = 1)
+
+  def add_item(title,price,quantity = 1)
     @price = price
-    @total += price * quantity
-    if quantity > 1
-      counter = 0
-      while counter < quantity
-        @items << item
-        counter += 1
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
       end
     else
-      @items << item
+      @items << title
     end
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
   end
 
-  def apply_discount
+  def apply_discount()
     if @discount > 0
-      @to_take_off = (price * discount)/100
-      @total -= @to_take_off
-      return "After the discount, the total comes to $#{total}."
+      @discount = @discount/100.to_f
+      @total = @total - (@total * (@discount))
+      "After the discount, the total comes to $#{@total.to_i}."
     else
-      return "There is no discount to apply."
+      "There is no discount to apply."
     end
   end
 
-  def void_last_transaction
+
+  def void_last_transaction()
     @total -= @price
   end
 
 end
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
